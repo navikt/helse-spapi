a2c4a81ec5f976e8f1072c25cfcdb388172f2fce
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-09-26 17:10:11 +0200

    Skiller request for Fellesordningen og Avtalefestet pensjon
    Så slipper vi å surre det til for Fellesordningen mens Avtalefestet pensjon skal ha fler versjoner.

--- src/main/kotlin/no/nav/helse/spapi/Konsument.kt
+++ src/main/kotlin/no/nav/helse/spapi/Konsument.kt
@@ -1,6 +1,10 @@
 package no.nav.helse.spapi
 
 import com.fasterxml.jackson.databind.JsonNode
+import no.nav.helse.spapi.personidentifikator.Personidentifikator
+import no.nav.helse.spapi.utbetalteperioder.UtbetaltPeriode
+import org.intellij.lang.annotations.Language
+import java.time.LocalDate
 
 internal abstract class Konsument(
     internal val navn: String,
@@ -9,7 +13,7 @@ internal abstract class Konsument(
     internal val behandlingsgrunnlag: Behandlingsgrunnlag
 ) {
     override fun toString() = navn
-    open suspend fun request(requestBody: JsonNode, versjon: Int): KonsumentRequest = RequiredOrganisasjonsnummerOptionalMinimumSykdomsgrad(requestBody)
+    abstract suspend fun request(requestBody: JsonNode, versjon: Int): KonsumentRequest
 
     internal companion object {
         internal val AlleKonsumenter = setOf(
@@ -31,52 +35,90 @@ internal object FellesordningenForAfp: Konsument(
     organisasjonsnummer = Organisasjonsnummer("987414502"),
     behandlingsnummer = "B709",
     behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object OsloPensjonsforsikring: Konsument(
-    navn = "Oslo pensjonsforsikring",
-    organisasjonsnummer = Organisasjonsnummer("982759412"),
+) {
+    override suspend fun request(requestBody: JsonNode, versjon: Int) = FellesordningenForAfpRequest(requestBody) // Fellesrdningen for AFP har ikke fler versjoner :)
+
+    internal class FellesordningenForAfpRequest private constructor(
+        override val fom: LocalDate,
+        override val tom: LocalDate,
+        override val personidentifikator: Personidentifikator,
+        private val organisasjonsnummer: Organisasjonsnummer,
+        private val minimumSykdomsgrad: Int?
+    ): KonsumentRequest {
+        internal constructor(requestBody: JsonNode): this(
+            fom = requestBody.periode.first,
+            tom = requestBody.periode.second,
+            personidentifikator = requestBody.personidentifikator,
+            organisasjonsnummer = requestBody.organisasjonsnummer,
+            minimumSykdomsgrad = requestBody.optionalMinimumSykdomsgrad
+        )
+
+        override fun filtrer(utbetaltePerioder: List<UtbetaltPeriode>) = utbetaltePerioder
+            .filter { it.organisasjonsnummer == organisasjonsnummer }
+            .filter { it.grad >= (minimumSykdomsgrad ?: 0) }
+
+        @Language("JSON")
+        override fun json(utbetaltPeriode: UtbetaltPeriode) = """
+            {
+              "fraOgMedDato": "${utbetaltPeriode.fom}",
+              "tilOgMedDato": "${utbetaltPeriode.tom}",
+              "tags": ${utbetaltPeriode.tags.map { "\"$it\"" }}
+              ${if (minimumSykdomsgrad == null) ",\"sykdomsgrad\": ${utbetaltPeriode.grad}" else ""}
+            }
+        """
+    }
+}
+
+internal abstract class AvtalefestetPensjon(navn: String, organisasjonsnummer: Organisasjonsnummer): Konsument(
+    navn = navn,
+    organisasjonsnummer = organisasjonsnummer,
     behandlingsnummer = "B709",
     behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object StatensPensjonskasse: Konsument(
-    navn = "Statens pensjonskasse",
-    organisasjonsnummer = Organisasjonsnummer("982583462"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object StorebrandLivsforsikring: Konsument(
-    navn = "Storebrand livsforsikring",
-    organisasjonsnummer = Organisasjonsnummer("958995369"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object KommunalLandspensjonskasse: Konsument(
-    navn = "Kommunal landspensjonskasse",
-    organisasjonsnummer = Organisasjonsnummer("938708606"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object StorebrandPensjonstjenester: Konsument(
-    navn = "Storebrand pensjonstjenester",
-    organisasjonsnummer = Organisasjonsnummer("931936492"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object GablerPensjonstjenester: Konsument(
-    navn = "Gabler pensjonstjenester",
-    organisasjonsnummer = Organisasjonsnummer("916833520"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object Aksio: Konsument(
-    navn = "Aksio",
-    organisasjonsnummer = Organisasjonsnummer("927613298"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
-internal object Nav: Konsument(
-    navn = "NAV",
-    organisasjonsnummer = Organisasjonsnummer("889640782"),
-    behandlingsnummer = "B709",
-    behandlingsgrunnlag = Behandlingsgrunnlag("GDPR Art. 6(1)e. AFP-tilskottsloven §17 første ledd, §29 andre ledd, første punktum. GDPR Art. 9(2)b")
-)
+) {
+    override suspend fun request(requestBody: JsonNode, versjon: Int): KonsumentRequest {
+        // TODO 1: Legge til en optional saksId i V1Request
+        // TODO 2: Lage en V2Request med required saksId
+        return AvtalefestetPensjonV1Request(requestBody)
+    }
+
+    internal class AvtalefestetPensjonV1Request private constructor(
+        override val fom: LocalDate,
+        override val tom: LocalDate,
+        override val personidentifikator: Personidentifikator,
+        private val organisasjonsnummer: Organisasjonsnummer,
+        private val minimumSykdomsgrad: Int?
+    ): KonsumentRequest {
+        internal constructor(requestBody: JsonNode): this(
+            fom = requestBody.periode.first,
+            tom = requestBody.periode.second,
+            personidentifikator = requestBody.personidentifikator,
+            organisasjonsnummer = requestBody.organisasjonsnummer,
+            minimumSykdomsgrad = requestBody.optionalMinimumSykdomsgrad
+        )
+
+        override fun filtrer(utbetaltePerioder: List<UtbetaltPeriode>) = utbetaltePerioder
+            .filter { it.organisasjonsnummer == organisasjonsnummer }
+            .filter { it.grad >= (minimumSykdomsgrad ?: 0) }
+
+        @Language("JSON")
+        override fun json(utbetaltPeriode: UtbetaltPeriode) = """
+            {
+              "fraOgMedDato": "${utbetaltPeriode.fom}",
+              "tilOgMedDato": "${utbetaltPeriode.tom}",
+              "tags": ${utbetaltPeriode.tags.map { "\"$it\"" }}
+              ${if (minimumSykdomsgrad == null) ",\"sykdomsgrad\": ${utbetaltPeriode.grad}" else ""}
+            }
+        """
+    }
+}
+
+internal object OsloPensjonsforsikring: AvtalefestetPensjon(navn = "Oslo pensjonsforsikring", organisasjonsnummer = Organisasjonsnummer("982759412"))
+internal object StatensPensjonskasse: AvtalefestetPensjon(navn = "Statens pensjonskasse", organisasjonsnummer = Organisasjonsnummer("982583462"))
+internal object StorebrandLivsforsikring: AvtalefestetPensjon(navn = "Storebrand livsforsikring", organisasjonsnummer = Organisasjonsnummer("958995369"))
+internal object KommunalLandspensjonskasse: AvtalefestetPensjon(navn = "Kommunal landspensjonskasse", organisasjonsnummer = Organisasjonsnummer("938708606"))
+internal object StorebrandPensjonstjenester: AvtalefestetPensjon(navn = "Storebrand pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("931936492"))
+internal object GablerPensjonstjenester: AvtalefestetPensjon(navn = "Gabler pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("916833520"))
+internal object Aksio: AvtalefestetPensjon(navn = "Aksio", organisasjonsnummer = Organisasjonsnummer("927613298"))
+
+//  Litt tøysete konsument som bare har tilgang i DEV for å teste selv
+internal object Nav: AvtalefestetPensjon(navn = "NAV", organisasjonsnummer = Organisasjonsnummer("889640782"))
--- src/main/kotlin/no/nav/helse/spapi/KonsumentRequest.kt
+++ src/main/kotlin/no/nav/helse/spapi/KonsumentRequest.kt
@@ -1,11 +1,8 @@
 package no.nav.helse.spapi
 
 import com.fasterxml.jackson.databind.JsonNode
-import io.ktor.server.application.*
-import io.ktor.server.request.*
 import no.nav.helse.spapi.personidentifikator.Personidentifikator
 import no.nav.helse.spapi.utbetalteperioder.UtbetaltPeriode
-import org.intellij.lang.annotations.Language
 import java.lang.IllegalArgumentException
 import java.time.LocalDate
 
@@ -15,37 +12,6 @@ internal interface KonsumentRequest {
     val personidentifikator: Personidentifikator
     fun filtrer(utbetaltePerioder: List<UtbetaltPeriode>): List<UtbetaltPeriode>
     fun json(utbetaltPeriode: UtbetaltPeriode): String
-}
-
-internal class RequiredOrganisasjonsnummerOptionalMinimumSykdomsgrad private constructor(
-    override val fom: LocalDate,
-    override val tom: LocalDate,
-    override val personidentifikator: Personidentifikator,
-    private val organisasjonsnummer: Organisasjonsnummer,
-    private val minimumSykdomsgrad: Int?
-): KonsumentRequest {
-
-    internal constructor(requestBody: JsonNode): this(
-        fom = requestBody.periode.first,
-        tom = requestBody.periode.second,
-        personidentifikator = requestBody.personidentifikator,
-        organisasjonsnummer = requestBody.organisasjonsnummer,
-        minimumSykdomsgrad = requestBody.optionalMinimumSykdomsgrad
-    )
-
-    override fun filtrer(utbetaltePerioder: List<UtbetaltPeriode>) = utbetaltePerioder
-        .filter { it.organisasjonsnummer == organisasjonsnummer }
-        .filter { it.grad >= (minimumSykdomsgrad ?: 0) }
-
-    @Language("JSON")
-    override fun json(utbetaltPeriode: UtbetaltPeriode) = """
-        {
-          "fraOgMedDato": "${utbetaltPeriode.fom}",
-          "tilOgMedDato": "${utbetaltPeriode.tom}",
-          "tags": ${utbetaltPeriode.tags.map { "\"$it\"" }}
-          ${if (minimumSykdomsgrad == null) ",\"sykdomsgrad\": ${utbetaltPeriode.grad}" else ""}
-        }
-    """
 }
 
 internal class UgyldigInputException(melding: String, cause: Throwable? = null): IllegalArgumentException(melding, cause)
@@ -63,13 +29,13 @@ private fun <T> JsonNode.optional(path: String, transformer: (jsonNode: JsonNode
         throw UgyldigInputException("Ugyldig verdi i feltet '$path' i request body: ${throwable.message} (verdien var ${jsonNode.asText()})")
     }
 }
-private val JsonNode.personidentifikator get() = required("personidentifikator") { Personidentifikator(it.asText()) }
-private val JsonNode.organisasjonsnummer get() = required("organisasjonsnummer") { Organisasjonsnummer(it.asText()) }
-private val JsonNode.periode get(): Pair<LocalDate, LocalDate> {
+internal val JsonNode.personidentifikator get() = required("personidentifikator") { Personidentifikator(it.asText()) }
+internal val JsonNode.organisasjonsnummer get() = required("organisasjonsnummer") { Organisasjonsnummer(it.asText()) }
+internal val JsonNode.periode get(): Pair<LocalDate, LocalDate> {
     val fom = required("fraOgMedDato") { LocalDate.parse(it.asText()) }
     val tom = required("tilOgMedDato") { LocalDate.parse(it.asText()).also { tom -> check(fom <= tom) { "Ugyldig periode $fom til $tom" } } }
     return fom to tom
 }
-private val JsonNode.optionalMinimumSykdomsgrad get() = optional("minimumSykdomsgrad") {
+internal val JsonNode.optionalMinimumSykdomsgrad get() = optional("minimumSykdomsgrad") {
     it.asInt().also { minimumSykdomsgrad -> check(minimumSykdomsgrad in 1..100) { "Må være mellom 1 og 100" } }
 }
