bbfd2a42fb5e7bea9918ef24ee447990812530df
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-01-24 10:40:44 +0100

    Nå er det kun SPK som er spesiell med å ikke kreve saksId

--- src/main/kotlin/no/nav/helse/spapi/Konsument.kt
+++ src/main/kotlin/no/nav/helse/spapi/Konsument.kt
@@ -61,7 +61,7 @@ internal object FellesordningenForAfp: Konsument(
     )
 }
 
-internal abstract class AvtalefestetPensjon(
+internal sealed class AvtalefestetPensjon(
     navn: String,
     organisasjonsnummer: Organisasjonsnummer,
     integrator: Organisasjonsnummer? = null,
@@ -75,7 +75,7 @@ internal abstract class AvtalefestetPensjon(
 ) {
     override suspend fun request(requestBody: JsonNode, versjon: Int) = AvtalefestetPensjonRequest(requestBody, saksId(requestBody))
 
-    abstract fun saksId(requestBody: JsonNode): SaksId?
+    open fun saksId(requestBody: JsonNode): SaksId? = requestBody.requiredSaksId
 
     internal class AvtalefestetPensjonRequest(requestBody: JsonNode, private val saksId: SaksId?): AfpRequest(
         fom = requestBody.periode.first,
@@ -103,26 +103,21 @@ internal object StatensPensjonskasse : AvtalefestetPensjon(
         else requestBody.optionalSaksId
     }
 }
-internal object OsloPensjonsforsikring: AvtalefestetPensjon(navn = "Oslo pensjonsforsikring", organisasjonsnummer = Organisasjonsnummer("982759412")) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
-internal object StorebrandLivsforsikring: AvtalefestetPensjon(navn = "Storebrand livsforsikring", organisasjonsnummer = Organisasjonsnummer("958995369")) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
-internal object KommunalLandspensjonskasse: AvtalefestetPensjon(navn = "Kommunal landspensjonskasse", organisasjonsnummer = Organisasjonsnummer("938708606")) {
-    private val prod = System.getenv().miljø == "prod"
-    override fun saksId(requestBody: JsonNode): SaksId? {
-        return if (prod) requestBody.requiredSaksId
-        else requestBody.optionalSaksId
-    }
-}
+internal object OsloPensjonsforsikring: AvtalefestetPensjon(navn = "Oslo pensjonsforsikring", organisasjonsnummer = Organisasjonsnummer("982759412"))
+internal object StorebrandLivsforsikring: AvtalefestetPensjon(navn = "Storebrand livsforsikring", organisasjonsnummer = Organisasjonsnummer("958995369"))
+internal object KommunalLandspensjonskasse: AvtalefestetPensjon(navn = "Kommunal landspensjonskasse", organisasjonsnummer = Organisasjonsnummer("938708606"))
+
 // Disse to er også integratorer (systemleverandører) slik som Aksio, men bruker ikke delegert tokens i maskinporten
 // Så de fremstår som pensjonskasser for oss på tross av at de ikke er det
-internal object StorebrandPensjonstjenester: AvtalefestetPensjon(navn = "Storebrand pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("931936492")) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
-internal object GablerPensjonstjenester: AvtalefestetPensjon(navn = "Gabler pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("916833520")) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
+internal object StorebrandPensjonstjenester: AvtalefestetPensjon(navn = "Storebrand pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("931936492"))
+internal object GablerPensjonstjenester: AvtalefestetPensjon(navn = "Gabler pensjonstjenester", organisasjonsnummer = Organisasjonsnummer("916833520"))
 
 private val Aksio = Organisasjonsnummer("927613298")
-internal object ArendalKommunalePensjonskasse: AvtalefestetPensjon(navn = "Arendal kommunale pensjonskasse", organisasjonsnummer = Organisasjonsnummer("940380014"), integrator = Aksio) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
-internal object DrammenKommunalePensjonskasse: AvtalefestetPensjon(navn = "Drammen kommunale pensjonskasse", organisasjonsnummer = Organisasjonsnummer("980650383"), integrator = Aksio) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
+internal object ArendalKommunalePensjonskasse: AvtalefestetPensjon(navn = "Arendal kommunale pensjonskasse", organisasjonsnummer = Organisasjonsnummer("940380014"), integrator = Aksio)
+internal object DrammenKommunalePensjonskasse: AvtalefestetPensjon(navn = "Drammen kommunale pensjonskasse", organisasjonsnummer = Organisasjonsnummer("980650383"), integrator = Aksio)
 
 //  Litt tøysete konsument som bare har tilgang i DEV for å teste selv
-internal object Nav: AvtalefestetPensjon(navn = "NAV", organisasjonsnummer = Organisasjonsnummer("889640782")) { override fun saksId(requestBody: JsonNode) = requestBody.requiredSaksId }
+internal object Nav: AvtalefestetPensjon(navn = "NAV", organisasjonsnummer = Organisasjonsnummer("889640782"))
 
 // Hvilke konsumenter som registreres når appen starter styres fra dev-nais.json & prod-nais.json i resouces.
 internal val AlleKonsumenter = setOf(FellesordningenForAfp, KommunalLandspensjonskasse, StatensPensjonskasse, StorebrandPensjonstjenester, StorebrandLivsforsikring, OsloPensjonsforsikring, GablerPensjonstjenester, ArendalKommunalePensjonskasse, DrammenKommunalePensjonskasse, Nav)
