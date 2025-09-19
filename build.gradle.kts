ff13a47f0a43c48fafd01a4ed2f106795131f0da
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-19 13:59:04 +0200

    Bumper tbd-libs
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val ktorVersion = "3.2.3"
-val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
+val tbdLibsVersion = "2025.09.19-13.31-61342e73"
 
 val logbackClassicVersion = "1.5.17"
 val logbackEncoderVersion = "8.0"
